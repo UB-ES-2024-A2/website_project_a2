79abb509ac791ff0d0119496a19a8f51c5aa9bb4
eloibusquetsgarcia-gonzalez <ebusquga7@alumnes.ub.edu>
2024-11-27 14:09:56 +0100

    feat: Set up requirements for testing

--- backend/app/tests/test_service.py
+++ backend/app/tests/test_service.py
@@ -1,10 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
-from app.main import app
 
-client = TestClient(app)
-
-def test_not_found_page():
+def test_not_found_page(client: TestClient) -> None:
     response = client.get("/not-found")
     assert response.status_code == 404 
     assert response.json() == {"detail": "Not Found"}  
