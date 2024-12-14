9c251edd041a5647d221ef253126101993c0716e
eloibusquetsgarcia-gonzalez <ebusquga7@alumnes.ub.edu>
2024-12-14 13:24:53 +0100

    fix: Get mybooks endpoint.

--- backend/app/api/routes/mybooks.py
+++ backend/app/api/routes/mybooks.py
@@ -2,7 +2,7 @@ from typing import Any
 from fastapi import APIRouter, HTTPException
 import mysql.connector
 
-from app.models import MyBookCreate, MyBookOut
+from app.models import MyBookCreate, MyBookOut, MyBooksOut, BooksOut
 from app.api.deps import SessionDep
 from app import crud
 
@@ -51,19 +51,32 @@ def delete_user_book(session: SessionDep, id_user: int, id_book: int) -> Any:
         if session.is_connected():
             cursor.close()
 
-@router.get("/mybooks/{id_user}/{id_book}", response_model=MyBookOut)
-def get_mybook(session: SessionDep, id_user: int, id_book: int) -> Any:
+@router.get("/{id_user}", response_model=BooksOut)
+def get_mybooks(session: SessionDep, id_user: int) -> Any:
     """
-    Get a book entry for a user based on user_id and book_id from the 'mybooks' table.
+    Get all books entry for a user based on user_id from the 'mybooks' table.
     """
     try:
         cursor = session.cursor()
-        mybook_out = crud.mybooks.get_mybooks(cursor=cursor, id_user=id_user, id_book=id_book)
+        mybook_out = crud.mybooks.get_mybooks(cursor=cursor, id_user=id_user)
         
         if not mybook_out:
             raise HTTPException(status_code=404, detail="Entry not found")
 
-        return mybook_out
+        # Inicializar una lista para los libros
+        books_data = []
+
+        # Obtener información de cada libro utilizando id_book de mybook_out
+        for mybook in mybook_out.data:
+            # Asumiendo que la función get_book_by_id devuelve un objeto con detalles del libro
+            book_details = crud.book.get_book_by_id(cursor=cursor, book_id=mybook.id_book)
+            if book_details:
+                books_data.append(book_details)
+            else:
+                print(f"Book with id {mybook.id_book} not found.")
+
+        # Crear y devolver la respuesta BooksOut con los libros obtenidos
+        return BooksOut(data=books_data, count=len(books_data))
 
     except mysql.connector.Error as e:
         print(f"Error connecting to MySQL: {e}")
