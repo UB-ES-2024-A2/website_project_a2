47a0b89fd9d521766c0f290cda89629a6ef96fb8
Marta <mbernapo49@alumnes.ub.edu>
2024-12-17 00:34:52 +0100

    User statistics

--- backend/app/api/routes/readbooks.py
+++ backend/app/api/routes/readbooks.py
@@ -1,8 +1,8 @@
-from typing import Any
+from typing import Any, List
 from fastapi import APIRouter, HTTPException
 import mysql.connector
 
-from app.models import ReadBookCreate, ReadBookOut
+from app.models import ReadBookCreate, ReadBookOut,BookOut
 from app.api.deps import SessionDep
 from app import crud
 
@@ -50,20 +50,25 @@ def delete_user_readbook(session: SessionDep, id_user: int, id_book: int) -> Any
     finally:
         if session.is_connected():
             cursor.close()
-
-@router.get("/readbooks/{id_user}/{id_book}", response_model=ReadBookOut)
-def get_readbook(session: SessionDep, id_user: int, id_book: int) -> Any:
+@router.get("/{id_user}", response_model=List[BookOut])
+def get_readbook(session: SessionDep, id_user: int) -> Any:
     """
     Get a book entry for a user based on user_id and book_id from the 'readbooks' table.
     """
     try:
         cursor = session.cursor()
-        readbook_out = crud.readbooks.get_readbook(cursor=cursor, id_user=id_user, id_book=id_book)
-        
+        readbook_out = crud.readbooks.get_readbook(cursor=cursor, id_user=id_user)
+
         if not readbook_out:
             raise HTTPException(status_code=404, detail="Entry not found")
 
-        return readbook_out
+        books_out = []
+        for readbook in readbook_out:
+            book_details = crud.book.get_book_by_id(cursor=cursor, book_id=readbook.id_book)
+            if book_details:
+                books_out.append(book_details)
+
+        return books_out
 
     except mysql.connector.Error as e:
         print(f"Error connecting to MySQL: {e}")
